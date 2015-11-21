f7b457d6bd0e7d8e90420cc68c8f92161ef7b81c
Samuel <samkidman@gmail.com>
2015-11-21 16:38:34 +1100

    create mibmodule spec

---
+++ test/mibmodule-spec.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Samuel on 21/11/2015.
+ */
+
+var MibModule = require('../src/parser/mibmodule.js');
+
+describe("MibModule constructor", function () {
+    var mibPath = "C:\\usr\\share\\snmp\\mibs\\IF-MIB.txt";
+    var ifMib = null;
+
+    beforeEach(function () {
+        ifMib = new MibModule(mibPath);
+    });
+
+    it("has a definitions property", function () {
+        expect(ifMib).to.have.property('definitions');
+    });
+
+    it("has a imports property", function () {
+        expect(ifMib.to.have.property('imports'));
+    });
+
+    describe("The definitions property", function () {
+       it("contains a mapping of identifiers to definitions");
+    });
+
+    describe("The imports property", function () {
+        it("contains a mapping of module identifiers to the list of symbols imported from that module");
+    })
+});
+
