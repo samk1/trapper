7c6da08b9a5340c08d80e51570bf8440f6b4847c
Samuel <samkidman@gmail.com>
2015-11-22 20:44:34 +1100

    define mib constants

---
+++ src/mib/mibconstants.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Samuel on 22/11/2015.
+ */
+
+exports.BUILTIN_TYPES = {
+    ObjectIdentifier: 'object_identifier',
+    OctetString: 'octet_string'
+};
+
+exports.DEFINED_MACROS = {
+    NotificationType: 'notification_type',
+    ModuleIdentity: 'module_identity',
+    ObjectIdentity: 'object_identity'
+};
