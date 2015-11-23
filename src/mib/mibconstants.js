a2e1cd82517428b9b74706cbe723b1902381d277
Samuel <samkidman@gmail.com>
2015-11-23 22:07:05 +1100

    add oid syntax constants

--- src/mib/mibconstants.js
+++ src/mib/mibconstants.js
@@ -12,3 +12,10 @@ exports.DEFINED_MACROS = {
     ModuleIdentity: 'module_identity',
     ObjectIdentity: 'object_identity'
 };
+
+exports.OID_SYNTAX_CLASSES = {
+    ModuleIdWithObjectName: 'module_id_with_object_name',
+    ModuleIdWithObjectNameAndIdentifierList: 'module_id_with_object_name_and_identifier_list',
+    IdentifierListOnly: 'identifier_list_only',
+    StringIdentifierListOnly: 'string_identifier_list_only'
+};
