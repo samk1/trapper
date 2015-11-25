6141072979aa87adf385bd8a9145105056403888
Samuel <samkidman@gmail.com>
2015-11-25 20:24:47 +1100

    symplified re-oidparser

--- src/mib/re-oidparser.js
+++ src/mib/re-oidparser.js
@@ -9,53 +9,37 @@ var identifierListMatch = `(?:${identifierMatch}\.)*${identifierMatch}`;
 
 var identifierListRe = new RegExp(`^\.?(${identifierListMatch})$`);
 var moduleObjectRe = new RegExp(`^(${moduleNameMatch})::(${objectNameMatch})\.(${identifierListMatch})?$`);
-var identifierRe = new RegExp(`(${identifierMatch})`, 'y');
-var numberRe = new RegExp(`^${numberMatch}$`);
 
 function parseIdentifierList(identifierList) {
-    var identifiers = [];
-    var identifier;
-    var matches;
-
-    while ( matches = identifierRe.exec(identifierList) ) {
-        if(numberRe.exec(matches[1])) {
-            identifier = parseInt(matches[1])
-        } else {
-            identifier = matches[1];
-        }
-
-        identifiers.push(identifier);
+    if(!identifierList) {
+        return null
     }
+
+    var identifiers = [];
+    var number;
+
+    identifierList.split('.').forEach(function (identifier) {
+        identifiers.push( number = parseInt(identifier) ? number : identifier );
+    });
 
     return identifiers;
 }
 
 function parse (string) {
     var matches;
-
     var syntax = null;
 
     if ( matches = identifierListRe.exec(string) ) {
-        var identifiers = parseIdentifierList(matches[1]);
-
         syntax = {
             syntax_class: 'identifier_list',
-            identifier_list: identifiers
+            identifier_list: parseIdentifierList(matches[1])
         };
     } else if ( matches = moduleObjectRe.exec(string) ) {
-        var moduleName = matches[1];
-        var objectName = matches[2];
-        var postIdentifiers = null;
-
-        if(matches[3]) {
-            postIdentifiers = parseIdentifierList(matches[3])
-        }
-
         syntax = {
             syntax_class: 'module_object',
-            module_name: moduleName,
-            object_name: objectName,
-            post_identifier_list: postIdentifiers
+            module_name: matches[1],
+            object_name: matches[2],
+            post_identifier_list: parseIdentifierList(matches[3])
         };
     }
 
