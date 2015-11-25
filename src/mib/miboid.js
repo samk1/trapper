aa8e7a9a4a5c93b5f270b2ebd9c7f36caad7c5ed
Samuel <samkidman@gmail.com>
2015-11-25 21:51:51 +1100

    simplifiy moboid object

--- src/mib/miboid.js
+++ src/mib/miboid.js
@@ -7,46 +7,12 @@ function MibOid(spec) {
         throw(new Error("Invalid MibOid specification"));
     }
 
-    var string = spec.string;
-    var identifiers = spec.identifiers;
-
-    var curIndex = 0;
-
-    Object.defineProperty(this, 'atEnd', {
-        get: function () {
-            return curIndex == identifiers.length
-        }
-    });
-
-    Object.defineProperty(this, 'nextIdentifier', {
-        get: function () {
-            return function () {
-                if(curIndex == identifiers.length) {
-                    return null;
-                }
-
-                return identifiers[curIndex++];
-            }
-        }
-    });
-
-    Object.defineProperty(this, 'reset', {
-        get: function () {
-            return function () {
-                curIndex = 0;
-            }
-        }
-    });
-
-    Object.defineProperty(this, 'identifiers', {
-        get: function () {
-            return identifiers.slice();
-        }
-    });
-
-    Object.defineProperty(this, 'string', {
-        value: string
-    });
+    this.string = spec.string;
+    this.identifiers = spec.identifiers.slice();
 }
+
+MibOid.prototype.identifiersCopy = function () {
+    return this.identifiers.slice()
+};
 
 exports.MibOid = MibOid;
--- test/miboid-spec.js
+++ test/miboid-spec.js
@@ -6,7 +6,7 @@ var MibOid = require('../src/mib/miboid.js').MibOid;
 var expect = require('expect.js');
 
 describe("MibOid prototype", function () {
-    describe("the identifiers property", function () {
+    describe("the identifiersCopy function", function () {
         var testMibOid = null;
 
         beforeEach(function () {
@@ -17,12 +17,12 @@ describe("MibOid prototype", function () {
         });
 
         it("returns an array of the identifiers in the oid", function () {
-            expect(testMibOid.identifiers).to.eql([ 1, 2, 3 ]);
+            expect(testMibOid.identifiersCopy()).to.eql([ 1, 2, 3 ]);
         });
 
-        it("creates a new array on get", function () {
-            testMibOid.identifiers.push(4);
-            expect(testMibOid.identifiers).to.eql([ 1, 2, 3 ]);
+        it("creates a new array", function () {
+            testMibOid.identifiersCopy().push(4);
+            expect(testMibOid.identifiersCopy()).to.eql([ 1, 2, 3 ]);
         });
     });
 
@@ -38,81 +38,6 @@ describe("MibOid prototype", function () {
 
         it("returns the string name of the oid", function () {
             expect(testMibOid.string).to.be("TEST-MIB::testObject");
-        });
-    });
-
-    describe("the nextIdentifier method", function () {
-        var testMibOid = null;
-
-        beforeEach(function () {
-            testMibOid = new MibOid({
-                identifiers: [ 1, 2, 3 ],
-                string: "TEST-MIB::testObject"
-            });
-        });
-
-        it("returns the first identifier on first call", function () {
-            expect(testMibOid.nextIdentifier()).to.be(1);
-        });
-
-        it("returns the second identifier on second call", function () {
-            testMibOid.nextIdentifier();
-            expect(testMibOid.nextIdentifier()).to.be(2);
-        });
-
-        it("returns the third identifier on third call", function () {
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-            expect(testMibOid.nextIdentifier()).to.be(3);
-        });
-
-        it("always returns null when there are no more identifiers", function () {
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-            expect(testMibOid.nextIdentifier()).to.be(null);
-            expect(testMibOid.nextIdentifier()).to.be(null);
-        });
-    });
-
-    describe("the reset method", function () {
-        var testMibOid = null;
-
-        beforeEach(function () {
-            testMibOid = new MibOid({
-                identifiers: [ 1, 2, 3 ],
-                string: "TEST-MIB::testObject"
-            });
-        });
-
-        it("moves the conceptual identifier pointer to the start", function () {
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-            testMibOid.reset();
-            expect(testMibOid.nextIdentifier()).to.be(1);
-        })
-    });
-
-    describe("the atEnd property", function () {
-        var testMibOid = null;
-
-        beforeEach(function () {
-            testMibOid = new MibOid({
-                identifiers: [ 1, 2, 3 ],
-                string: "TEST-MIB::testObject"
-            });
-        });
-
-        it("is false if a call to nextIdentifier will not return null", function () {
-            expect(testMibOid.atEnd).to.be(false);
-        });
-
-        it("is true if a call to nextIdentifier will return null", function () {
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-            testMibOid.nextIdentifier();
-
-            expect(testMibOid.atEnd).to.be(true);
         });
     });
 });
