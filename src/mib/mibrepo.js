a449a8f5f172ebb8adf265eec4accc866267b444
Samuel <samkidman@gmail.com>
2015-12-02 21:22:15 +1100

    finally worked out how to build the object tree properly. should be able to do everything snmptranslate can do using this technique.

--- src/mib/mibrepo.js
+++ src/mib/mibrepo.js
@@ -13,10 +13,159 @@ var path = require('path');
 function MibRepo(search) {
     var mibModules = {};
 
-    var mibObjectTree = new MibObjectTree();
+    var rootObject = new MibObject({
+        id: 1,
+        parent: null,
+        name: 'iso'
+    });
+
+    var descriptors = {
+        iso: [{
+            oid: [ 1 ],
+            from: 'SNMPv2-SMI'
+        }]
+    };
+
+    var oids = {
+        '.1': {
+            descriptor: 'iso',
+            from: 'SNMPv2-SMI'
+        }
+    };
+
+    function Tree() {
+        var self = this;
+        var isoObject = null;
+
+        var isoNode = {
+            _object: isoObject
+        };
+
+        function addObject(object, oid) {
+            var currentNode = self.root;
+            var prevNode = null;
+
+            oid.forEach(function (identifier) {
+                if(!currentNode[identifier]) {
+                    currentNode[identifier] = {}
+                }
+
+                prevNode = currentNode;
+                currentNode = currentNode[identifier];
+            });
+
+            prevNode[object.descriptor] = currentNode;
+            currentNode._object = object
+        }
+
+        this.root = {
+            1: isoNode,
+            iso: isoNode
+        };
+        this.addObject = addObject;
+    }
+
+    var tree = new Tree();
+
+    function expandOids() {
+        function addDescriptor(descriptor, oid, from) {
+            if(!descriptors[descriptor]) {
+                descriptors[descriptor] = [ {
+                    oid: oid,
+                    from: from
+                } ];
+            } else {
+                descriptors[descriptor].push({
+                    oid: oid,
+                    from: from
+                });
+            }
+        }
+
+        function findOid(descriptor, from) {
+            if(!descriptors[descriptor]) {
+                return null;
+            }
+
+            descriptors[descriptor].forEach(function (defn) {
+                if(defn.from === from) {
+                    return defn.oid;
+                }
+            })
+        }
+
+        function expandOid(objectDescriptor, module) {
+            var moduleName = module.name;
+            var oid = findOid(objectDescriptor, moduleName);
+
+            if(typeof oid[0] === 'number') {
+                return oid;
+            }
+
+            var baseDescriptor = oid.unshift();
+            var from;
+            if(module.importsDescriptor(baseDescriptor)) {
+                from = module.getExporterForDescriptor(baseDescriptor);
+            } else {
+                from = moduleName;
+            }
+
+            return oid.concat(expandOid(baseDescriptor, mibModules[from]))
+        }
+
+        // We examine every object that has been defined in two passes. The first pass can
+        // create some extra objects which necessitates the second pass.
+
+        // First pass: extract all partial ids in this format [foo, n_1, n_2, ... , n_m]
+        // If there are any 'bare' descriptors (eg 'bar' in [foo, bar(2), 1]) these are
+        // added to the module as an extra object.
+        Object.keys(mibModules).forEach(function (moduleName) {
+            var module = mibModules[moduleName];
+
+            Object.keys(module.objects).forEach(function (objectDescriptor) {
+                var object = module[objectDescriptor];
+                var oidSyntax = object.oidSyntax.slice();
+                var oid = [];
+                do {
+                    var oidElm = oidSyntax.pop();
+                    oid.push(oidElm.id || 0);
+                } while (oidSyntax.length > 1);
+
+                oid.push(oidSyntax.pop().de);
+
+                addDescriptor(objectDescriptor, oid, moduleName);
+
+                for(var i = 1; i++; i < object.oidSyntax.length) {
+                    var descriptor;
+                    if(descriptor = object.oidSyntax[1].de) {
+                        addDescriptor(descriptor, oid.slice(0, i+1), moduleName);
+                    }
+                }
+            });
+        });
+
+        // Second pass: expand all oids that were extracted above into full format
+        // starting from root (eg .1.3.6.1.2.1.2.2 and so on), and then insert the object
+        // into the tree for quick retrieval.
+        Object.keys(mibModules).forEach(function (moduleName) {
+            var module = mibModules[moduleName]; {
+                Object.keys(module.objects).forEach(function (objectDescriptor) {
+                    var object = module.objects[objectDescriptor];
+
+                    if(object.hasOid) {
+                        return;
+                    }
+
+                    var oid = expandOid(objectDescriptor, module);
+
+                    tree.addObject(object, oid)
+                });
+            }
+        });
+    }
 
     function getMibObjectData(mibOid) {
-        return mibObjectTree.getObject(mibOid.identifiersCopy()).getData();
+        return objectTree.getObject(mibOid.identifiersCopy()).getData();
     }
 
     function parseModuleObjectOidSyntax(oidSyntax) {
@@ -35,14 +184,14 @@ function MibRepo(search) {
         }
 
         //Trace to root
-        var oidDescriptors = mibObjectTree.getDescriptorPath(object);
-        var oidIdentifiers = mibObjectTree.getIdentifierPath(object);
+        var oidDescriptors = objectTree.getDescriptorPath(object);
+        var oidIdentifiers = objectTree.getIdentifierPath(object);
 
         if (postIdentifiers) {
             var postIdentifier;
 
             while (postIdentifier = postIdentifiers.pop()) {
-                if (object = mibObjectTree.getChildObject(object, postIdentifier)) {
+                if (object = objectTree.getChildObject(object, postIdentifier)) {
                     oidIdentifiers.push(object.identifier);
                     oidDescriptors.push(object.descriptor);
                 }
@@ -70,12 +219,12 @@ function MibRepo(search) {
         var oidIdentifiers = [];
         var identifiers = oidSyntax.identifier_list;
         var identifier;
-        var object;
+        var object = rootObject;
 
         //It's *almost* the same as getMibObjectData
         //It's exactly the same as the loop in parseModuleObjectOidSyntax
         while (identifier = identifiers.pop()) {
-            if (object = mibObjectTree.getChildObject(object, identifier)) {
+            if (object = objectTree.getChildObject(object, identifier)) {
                 oidIdentifiers.push(object.identifier);
                 oidDescriptors.push(object.descriptor);
             }
@@ -131,34 +280,6 @@ function MibRepo(search) {
         return filePaths;
     }
 
-    function buildObjectTree() {
-        Object.keys(mibModules).forEach(function(moduleName) {
-            var module = mibModules[moduleName];
-
-            Object.keys(module.objects).forEach(function(objectDescriptor) {
-                var path = [];
-                var object = module.objects[objectDescriptor];
-                var parentDescriptor;
-
-                 do {
-                    object.oidPart.forEach(function(element) {
-                        path.unshift(element)
-                    });
-
-                    parentDescriptor = object.oidValue[0].descriptor;
-                    if(module.importsDescriptor(parentDescriptor)) {
-                        var parentModuleName = module.getExporterForDescriptor(parentDescriptor);
-                        object = mibModules[parentModuleName].objects[parentDescriptor];
-                    } else {
-                        object = module.objects[parentDescriptor];
-                    }
-                } while(!atRoot(object));
-
-                mibObjectTree.addObject(path, object);
-            });
-        })
-    }
-
     //Parse mib modules in search paths
     if (Array.isArray(search)) {
         search.forEach(function (dirPath) {
@@ -168,8 +289,8 @@ function MibRepo(search) {
         parseMibs(search)
     }
 
-    //Makes a path from all mib objects in all modules to rootObject (.1)
-    buildObjectTree();
+    //Expand all OIDs
+    expandOids();
 
     Object.defineProperty(this, 'getMibObjectData', {
         value: getMibObjectData
